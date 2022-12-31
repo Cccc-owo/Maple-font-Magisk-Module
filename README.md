fc8470b0ec90131781dfcd65cfa83587894f8107
Cccc-owo <net4hhhh@163.com>
2022-12-31 22:53:12 +0800

    init: v1.0.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# Maple Font - Magisk 字体替换模块
+
+## 使用
+
+在 [GitHub Action](https://github.com/Cccc-owo/Maple-font-Magisk-Module/actions) 的最新构建中下载 artifact
+
+*注意，在 GitHub Action 中下载到的压缩包（artifact.zip）须再将其内部的 ```Maple-font-Magisk-Module.zip``` 提取出来刷入。*
+
+或在 [Release](https://github.com/Cccc-owo/Maple-font-Magisk-Module/releases/latest) 下载并在 Magisk 中刷入 ```Maple-font-Magisk-Module.zip```。（推荐） 
+
+建议与[「遍黑体」外挂补全 Magisk 模块](https://github.com/lxgw/Plangothic-magisk-module)一起使用
+
+## 引用
+
+|/|版本|
+|-|-|
+|[CJK Font Magisk Module Template](https://github.com/lxgw/advanced-cjk-font-magisk-module-template)|v3.3.220605|
+|[Maple-font](https://github.com/subframe7536/Maple-font)|v6.0|
+|[7-zip](https://7-zip.org/)|v22.01|
+|[wget](https://eternallybored.org/misc/wget/)|v1.21.3|
---
+++ main.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+set font_zip=https://github.com/subframe7536/Maple-font/releases/download/v6.0/MapleMono-SC-NF.zip
+set template_zip=https://github.com/lxgw/advanced-cjk-font-magisk-module-template/releases/download/v3.3.220605/FontTemplateA-Magisk204.zip
+::checking
+ECHO Time:%date%
+ECHO Checking the required folder...
+if not exist output\ ( mkdir output\ ) else ( del /F /S /Q output\ & rmdir /S /Q output\ & mkdir output\ )
+if exist Maple-font-Magisk-Module.zip del /F /S /Q Maple-font-Magisk-Module.zip
+if exist fonts\ del /F /S /Q fonts\ & rmdir /S /Q fonts\
+::integrating files
+ECHO.
+wget.exe -O "font.zip" %font_zip%
+wget.exe -O "Template.zip" %template_zip%
+7z.exe e font.zip SC-NF\*.ttf -r -o.\fonts
+7z.exe x Template.zip -r -o.\output
+xcopy fonts\*.ttf output\system\fonts /E /Q /C /Y
+
+ren output\system\fonts\MapleMono-SC-NF-Light.ttf fontch3.ttf
+ren output\system\fonts\MapleMono-SC-NF-Regular.ttf fontch4.ttf
+ren output\system\fonts\MapleMono-SC-NF-Bold.ttf fontch5.ttf
+
+del output\system\fonts\MapleMono-SC-NF-LightItalic.ttf
+del output\system\fonts\MapleMono-SC-NF-Italic.ttf
+del output\system\fonts\MapleMono-SC-NF-BoldItalic.ttf
+
+del /F /S /Q output\module.prop
+xcopy module.prop output\
+ECHO.
+ECHO Compressing the generated files...
+7z.exe a Maple-font-Magisk-Module.zip .\output\*
+::done
+ECHO.
+ECHO End of script running.
